c825f5397f19e2ecf6459f69a6a4d685ecc9bf36
OneSpir1t <114656266+OneSpir1t@users.noreply.github.com>
2024-09-04 08:28:59 +0500

    feat: logic & design

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const CurrentState = {};
 
-chrome.storage.local.get(["WikiHelperisActive"]).then((result) => {
+var orphanMessageId = chrome.runtime.id + 'orphanCheck';
+window.dispatchEvent(new Event(orphanMessageId));
+window.addEventListener(orphanMessageId, unregisterOrphan);
+chrome.runtime.onMessage.addListener(onMessage);
+
+chrome.storage.local.get(["WikiHelperIsCutActive"]).then((result) => {
     Object.assign(CurrentState, result)
-    console.log(CurrentState?.WikiHelperisActive, 'WikiHelperisActive');
-    
-    if (CurrentState?.WikiHelperisActive) {
-        addEventOnCopy()
-    }
+    if (CurrentState?.WikiHelperIsCutActive) addEventOnCopy()
 });
 
-function handleCopyEvent(e) {
+function CopyEventHandler(e) {
+    // DOM events still fire in the orphaned content script after the extension
+    // was disabled/removed and before it's re-enabled or re-installed
+    if (unregisterOrphan()) { return }
     var text = window.getSelection().toString().trim();
     e.clipboardData.setData('text/plain', text);
     e.preventDefault();
 }
 
-function addEventOnCopy() {
-    document.addEventListener('copy', handleCopyEvent);
-}
+function addEventOnCopy() { document.addEventListener('copy', CopyEventHandler) }
 
-function removeEventOnCopy() {
-    document.removeEventListener('copy', handleCopyEvent);
-}
+function removeEventOnCopy() { document.removeEventListener('copy', CopyEventHandler) }
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.WikiHelperisActive) addEventOnCopy()
+function onMessage(message, sender, sendResponse) {
+    if (message.WikiHelperIsCutActive) addEventOnCopy()
     else removeEventOnCopy();
-});
+}
+
+function unregisterOrphan() {
+    if (chrome.runtime.id) {
+      // someone tried to kick us out but we're not orphaned! 
+      return;
+    }
+    window.removeEventListener(orphanMessageId, unregisterOrphan);
+    removeEventOnCopy();
+    try {
+      // 'try' is needed to avoid an exception being thrown in some cases 
+      chrome.runtime.onMessage.removeListener(onMessage);
+    } catch (e) {}
+    return true;
+  }
--- popup.js
+++ popup.js
@@ -1,27 +1,95 @@
 const CurrentState = {};
+const controls = {};
 
-chrome.storage.local.get(["WikiHelperisActive"]).then((result) => {
-    Object.assign(CurrentState, result)
-    const checkbox = document.getElementById('WikiHelperisActive');
-    
-    CurrentState?.WikiHelperisActive ? checkbox.checked = true : checkbox.checked = false;
-    console.log(CurrentState);
-    console.log(CurrentState?.WikiHelperisActive, 'WikiHelperisActive');
-    
-    checkbox.addEventListener('change', function() {
-        chrome.storage.local.set({ WikiHelperisActive: this.checked })
-        if(this.checked) {
-            sendMsg({WikiHelperisActive: true});
-        }
-        else {
-            sendMsg({WikiHelperisActive: false});
-        }
+document.addEventListener('DOMContentLoaded', () => {
+
+    setControls();
+
+    if(!controls.content) return;
+
+    chrome.storage.local.get(["WikiHelperIsCutActive", "WikiHelperIsToggled"]).then((result) => {
+        Object.assign(CurrentState, result);
+        ManiSettingsInit();
+        versionSettingInit();
+        hidePreLoader();
     });
-
 });
 
-async function sendMsg(message) {
-    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    const response = await chrome.tabs.sendMessage(tab.id, message);
-    console.log(response);
+function sendMsg(message) {
+    chrome.tabs.query({ url: "http://wiki.lex.lan/*" }).then((tabQuery) => {
+        if(!tabQuery || !isValidChromeRuntime()) return;
+        tabQuery.forEach(element => {
+            chrome.tabs.sendMessage(element.id, message);    
+        });
+
+    });
+}
+
+function ManiSettingsInit() {
+    CurrentState?.WikiHelperIsCutActive ? controls.Cutcheckbox.checked = true : controls.Cutcheckbox.checked = false;
+    controls.Cutcheckbox.addEventListener('change', CutcheckboxEventHandler);
+}
+
+function versionSettingInit(){
+    if (CurrentState?.WikiHelperIsToggled) {
+        controls.toggleButton.textContent = 'Развернуть';
+        controls.versionsDiv.classList.toggle('collapsed');
+    }
+    controls.toggleButton.addEventListener('click', toggleButtonEventHandler);
+}
+
+function CutcheckboxEventHandler(){
+    chrome.storage.local.set({ WikiHelperIsCutActive: this.checked })
+    if(this.checked) sendMsg({ WikiHelperIsCutActive: true });
+    else sendMsg({ WikiHelperIsCutActive: false });
+}
+
+function toggleButtonEventHandler() {
+    controls.versionsDiv.classList.toggle('collapsed');
+
+    // Changing the button text depending on the block status
+    if (controls.versionsDiv.classList.contains('collapsed')) controls.toggleButton.textContent = 'Развернуть';
+    else controls.toggleButton.textContent = 'Свернуть';
+
+    if (controls.toggleButton.textContent === 'Развернуть') {
+        chrome.storage.local.set({ WikiHelperIsToggled: true })
+    }
+    else chrome.storage.local.set({ WikiHelperIsToggled: false })
+}
+
+function isValidChromeRuntime() {
+    return chrome.runtime && !!chrome.runtime.getManifest();
+}
+
+async function setControls() {
+    const Cutcheckbox = document.getElementsByClassName('WikiHelperIsCutActive')[0];
+    const toggleButton = document.getElementById('toggleButton');
+    const versionsDiv = document.getElementById('versionsDiv');
+    const preloader = document.getElementById("preloader");
+    const content = document.getElementById("mainContent");
+    const preloaderContainer = document.getElementById("preloaderContainer");
+    const controlsToAdd = {};
+    controlsToAdd.Cutcheckbox = Cutcheckbox;
+    controlsToAdd.toggleButton = toggleButton;
+    controlsToAdd.versionsDiv = versionsDiv;
+    controlsToAdd.preloader = preloader;
+    controlsToAdd.content = content;
+    controlsToAdd.preloaderContainer = preloaderContainer;
+    Object.keys(controlsToAdd).forEach(key => {
+        if (!(key in controls)) {
+            controls[key] = controlsToAdd[key];
+        } else {
+            console.warn(`Property ${key} already exists in controls.`);
+        }
+    });
+}
+
+function hidePreLoader() {
+    if (controls.preloader) {
+        controls.preloader.style.display = "none";
+        controls.preloaderContainer.classList.add('collapsed');
+    }
+    if (controls.content) {
+        controls.content.classList.remove("hidden");
+    }
 }
